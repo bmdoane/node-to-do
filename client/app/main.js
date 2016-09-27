e1f0a5cdb35400da98ecd17cd5ff1b409e6c18bf
Brian Doane <brianmdoane@gmail.com>
2016-09-26 19:13:07 -0500

    getting data from a node call to the dom

--- client/app/main.js
+++ client/app/main.js
@@ -1,6 +1,10 @@
 'use strict'
 
 angular.module('nodeToDo', [])
-	.controller('main', function($scope) {
-		$scope.title = 'Node ToDo'
+	.controller('main', function($scope, $http) {
+		$http
+			.get('/api/todo')
+			.then(({ data: { todo }}) =>
+				$scope.todo = todo
+			)
 	})
--- server/server.js
+++ server/server.js
@@ -11,5 +11,10 @@ const PORT = process.env.PORT || 3000
 // Middlewares
 app.use(express.static('client'))
 
+// Routing
+app.get('/api/todo', (req, res) =>
+	res.json({ todo: 'Pick up steaks' })
+)
+
 // Listen to requests on the provided port and log when available
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
