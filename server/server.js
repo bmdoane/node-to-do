320f90a08c813ee962631e9320b7a1af63815522
Brian Doane <brianmdoane@gmail.com>
2016-09-26 23:30:09 -0500

    get messages from mongo

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 'use strict'
 
 const express = require('express')
+const mongoose = require('mongoose')
 
 // Initialize
 const app = express()
 
 // Configure
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/nodetodo'
 const PORT = process.env.PORT || 3000
 
 // Middlewares
@@ -16,22 +18,21 @@ app.get('/api/title', (req, res) =>
 	res.json({ title: 'Things I need to do...' })
 )
 
-app.get('/api/todos', (req, res) =>
-  res.json({ 
-  	todos: [
-  		{
-  			task: 'Pick up steaks on the way home',
-  			dueDate: 'Wednesday',
-  			assignedTo: 'Brian',
-  		},
-  		{
-  			task: 'Take the fridge for a walk',
-  			dueDate: 'Friday',
-  			assignedTo: 'Brian',
-  		}
-  	]
-  })
+const ToDo = mongoose.model('todo', {
+	task: String,
+	dueDate: String,
+	assignedTo: String,
+})
+
+app.get('/api/todos', (req, res, err) =>
+	ToDo
+		.find()
+		.then(todos => res.json({ todos }))
+		.catch(err)
 )
 
-// Listen to requests on the provided port and log when available
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+mongoose.Promise = Promise
+mongoose.connect(MONGODB_URL, () =>
+	// Listen to requests on the provided port and log when available
+	app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+)	
